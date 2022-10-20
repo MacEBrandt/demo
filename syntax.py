fbadf37f3603c6a0fca4d1cff1f49248040db942
MacEBrandt <116313596+MacEBrandt@users.noreply.github.com>
2022-10-20 22:00:53 +0000

    Added code to syntax, fixed replit

---
+++ syntax.py
@@ -0,0 +1,15 @@
+print("hello world")
+
+print("")
+# new line 
+print("I\nwant\na\nnew\nline")
+print("")
+name = input("what is your name? ")
+print("Hello ", name)
+
+def list():
+  list1 = [1,2,3,4,5,6,7]
+  i = 0
+  while (i<(len(list1))):
+    print(list1[i])
+    i += 1
